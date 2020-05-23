2d9d130fce1543cb5e36eb40e0badc900ca1bd11
elmissouri16 <elmissouri16@gmail.com>
2020-05-23 07:00:09 +0000

    fix req not returning json

--- app.py
+++ app.py
@@ -4,4 +4,4 @@ app = Flask(__name__)
 @app.route('/', methods=['GET'])
 def getme():
     rsp = requests.get('https://api.kanye.rest/?format=json')
-    return jsonify(rsp.content)
+    return jsonify(rsp.json())
